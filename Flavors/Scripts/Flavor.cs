d77260a301413d2bc46fdaf23e2a6ea25da70c03
mikantic <mikedwenning@gmail.com>
2025-10-12 12:12:38 -0400

    setup flavor classes

---
+++ Flavors/Scripts/Flavor.cs
@@ -0,0 +1,82 @@
+namespace Core.Flavor
+{
+    /// <summary>
+    /// interface for a flavor
+    /// </summary>
+    public interface IFlavor
+    {
+
+    }
+    
+    /// <summary>
+    /// basic flavor
+    /// </summary>
+    public class Basic : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// savory flavor
+    /// </summary>
+    public class Savory : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// spicy flavor
+    /// </summary>
+    public class Spicy : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// salty flavor
+    /// </summary>
+    public class Salty : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// sweet flavor
+    /// </summary>
+    public class Sweet : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// bitter flavor
+    /// </summary>
+    public class Bitter : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// minty flavor
+    /// </summary>
+    public class Minty : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// sour flavor
+    /// </summary>
+    public class Sour : IFlavor
+    {
+
+    }
+
+    /// <summary>
+    /// rotten flavor
+    /// </summary>
+    public class Rotten : IFlavor
+    {
+
+    }
+}
